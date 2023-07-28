42daf470c0559b7d05d755f7fb1be1727975d38e
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-07-28 23:56:25 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const ulElement = document.querySelector('.gallery');
+
+ulElement.innerHTML = makeGalleryMarkup(galleryItems);
+lightboxRun();
+
+// Створюємо розмітку
+function makeGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+// Функція запуску плагіна
+function lightboxRun() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    overlay: true,
+    overlayOpacity: 0.7,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
+
